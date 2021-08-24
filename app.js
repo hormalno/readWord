c0732c29d86d0452a8da1b2c870abd2ab3904062
hormalno <ivanov.yasen@gmail.com>
2021-08-24 18:20:05 +0300

    updated q're

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const readWord = require('./tools/readWord.js').readWord;
 const writeMDD = require('./tools/writeMDD.js').writeMDD;
 
 
-readWord('C:/Users/yasen.ivanov/Documents/readWord_v2/readWord/modules/AIRT/AIRT_');
-writeMDD('C:/Users/yasen.ivanov/Documents/readWord_v2/readWord/modules/AIRT/AIRT_');
+readWord('C:/Users/yasen.ivanov/Documents/readWord_v2/readWord/modules/CHEM/CHEM_');
+writeMDD('C:/Users/yasen.ivanov/Documents/readWord_v2/readWord/modules/CHEM/CHEM_');
