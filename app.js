76151fc88a458208168da3d5f2a3787d1452ec2d
hormalno <ivanov.yasen@gmail.com>
2021-08-25 17:23:50 +0300

    added error log

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const readWord = require('./tools/readWord.js').readWord;
 const writeMDD = require('./tools/writeMDD.js').writeMDD;
 
+const moduleName = "MECH"
 
-readWord('C:/Users/yasen.ivanov/Documents/readWord_v2/readWord/modules/MECH/MECH_');
-writeMDD('C:/Users/yasen.ivanov/Documents/readWord_v2/readWord/modules/MECH/MECH_');
+const modulePath = "C:/Users/yasen.ivanov/Documents/readWord_v2/readWord/modules/"+moduleName+"/"+moduleName+"_"
+
+readWord(modulePath);
+writeMDD(modulePath);
--- tools/writeMDD.js
+++ tools/writeMDD.js
@@ -7,7 +7,7 @@ module.exports = {
 
         const controlData = require('./controlData.js');
 
-        var questions = controlData.readJSON(modulePath);
+        const questions = controlData.readJSON(modulePath);
 
         //write metadata of the module
         var writeMetadata = fs.createWriteStream(modulePath+'metadata.txt', {flags: 'w',encoding: 'utf-8'})
@@ -20,12 +20,20 @@ module.exports = {
 
         // write routing of the module
         var writeRouting = fs.createWriteStream(modulePath+'routing.txt', {flags: 'w', encoding: 'utf-8'})
+        var writeRoutingErrors = fs.createWriteStream(modulePath+'routingErrors.txt', {flags: 'w',encoding: 'utf-8'})
+        
 
-        questions.forEach(question => {
-            let questionStr = writeQuestionRouting(question);
-            writeRouting.write(questionStr)
+        questions.forEach((question,i,questions) => {
+            let questionStr = writeQuestionRouting(question,questions);
+            if (questionStr.error) {
+                writeRouting.write(questionStr.message);
+                writeRoutingErrors.write(questionStr.message);
+            } else {
+                writeRouting.write(questionStr);  
+            }
         });
         writeRouting.end() // close string
+        writeRoutingErrors.end()
 
         console.log('This is after the read call');
 
@@ -65,25 +73,62 @@ function writeQuestionMetadata(question) {
     return result;
 }
 
-function writeQuestionRouting(question) {
-    let categoryLetter = ['A','B','C','D','E','F','G','H','I','J','K','L'];
+function writeQuestionRouting(question,questions) {
+    const categoryLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'];
+    const questionNames = questions.map(x=>x.qname);
 
-    if (question.routing.indexOf('jobcat') > -1) {
-        let heading = "'" + question.qname + "\n"
-        heading += question.qname + '.Ask()\n'
-        return heading;
+    const errValidation = validateRouting(question,questionNames);
+    if (errValidation) {
+       
+        return {error:true, message:errValidation};
+    }
+
+    let routing = question.routing.split('#qstart#')[0].trim();
+
+    if (routing.indexOf('jobcat') > -1) {
+        return"'" + question.qname + "\n" + question.qname + '.Ask()\n';
     } else {
-        let routing = 'if ' + question.routing.split('#qstart#')[0].trim().replace(/\s*=\s*/,' * {_') + '} then\n';
+        routing = 'if ' + routing.replace(/\s*=\s*/,' * {_') + '} then\n';
 
         if (routing.indexOf('_check') > -1) {
-            var catValue = routing.match(/\{_([\d])+\}/)[1];
-            routing = routing.replace(/\{_[\d]+\}/,'{'+ categoryLetter[catValue-1] +'}')
-            
+            var catValue = routing.match(/\{_([\d]+)\}/)[1];
+            if (!categoryLetter[catValue-1]) {
+                routing = routing.replace('a','b');
+            }
+            routing = routing.replace(/\{_[\d]+\}/,'{'+ categoryLetter[catValue-1] +'}')       
         }
 
-        routing += '\t' + question.qname + '.Ask()\n'
-        routing += 'end if\n';
-        return routing;
+        return routing + '\t' + question.qname + '.Ask()\nend if\n';
     }
+    
+}
+
+function validateRouting(question,questionNames) {
+
+    
+    let routing = question.routing.split('#qstart#')[0].trim();
+
+    // check if its follwoing the instruction layout
+        if (routing.indexOf('=') < 0) {
+            return "'to do Incorrect instruction (doesnt have = symbol)\n'" + "if " + routing + " then\n'\t" + question.qname + ".Ask()\n'end if\n"
+        }
+
+        if (routing.indexOf('_radio') < 0 && routing.indexOf('_check') < 0 && routing.indexOf('jobcat') < 0) {
+            return "'to do Incorrect instruction (the question name is incorrect)\n'" + "if " + routing + " then\n'\t" + question.qname + ".Ask()\n'end if\n"
+        }
+
+    // check if question exists
+        if (routing.indexOf('jobcat') < 0) {
+            if(questionNames.filter((qname) => qname.toLowerCase() === routing.split('=')[0].trim()).length === 0) {
+                return "'to do question doesnt exists\n'" + "if " + routing + " then\n'\t" + question.qname + ".Ask()\n'end if\n"
+            }
+        }
+
+    // check if the answer has value
+        if(!question.routing.split('=')[1].trim()) {
+            return "'to do check instruction\n'" + "if " + routing + " then\n'\t" + question.qname + ".Ask()\n'end if\n"
+        }
+
+    
 }
 
